87ca0000c29a7fd063bff2899ea4eb782501e7c3
Placideh <h.uwizeyeplacide@gmail.com>
2020-07-28 10:34:48 +0200

    selectionSort

--- SelectionSort.java
+++ SelectionSort.java
@@ -6,24 +6,26 @@ public class SelectionSort{
 	
 	}
 	public static int[]myArr(int[]arr){
+		// int j=0;
+		for (int k=0;k<arr.length ;k++ ) {
 		int i=0;
+		int minPos=i;
 		int j=0;
-		int zeroIndex=i-1;
-		for (j=zeroIndex+j;j<arr.length ;j++ ) {
-				zeroIndex=j;
-			if (arr[zeroIndex]>arr[j]) {
-				int temp=arr[zeroIndex];
-				arr[zeroIndex]=arr[j];
+		for (j=minPos+j;j<arr.length-1 ;j++ ) {
+			minPos=j+1;
+			if (arr[j]>arr[j+1]) {
+				int temp=arr[minPos];
+				arr[minPos]=arr[j];
 				arr[j]=temp;
 				
 			}
-			
 
+		}
 		}
 		return arr;
 
-
-
-
 	}
 }
+/*ACTUALLY THIS SELECTION I HAVE IMPLEMENTED BY MYSELF AND IT IS NOT THAT MUCH 
+SCARY BUT IT TAKES ME LIKE 2 HOURS BUT FINALLY I HAVE COME TO FIGURE OUT SO 
+THIS IS PROGRAMMING YOU HAVE TO KEEP TRYING TILL THE THINGS TURN TO BE GOOD*/
